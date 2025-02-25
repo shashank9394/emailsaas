dad37d496a0e8f964e232e094b18211126f445c4
Shashank <rajashashank44@gmail.com>
2025-02-25 10:03:53 +0530

    EmailPro

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,7 +1,23 @@
-export const POST = async (req: Request) => {
-    const { data } = await req.json()
-        console.log('clerk webhook received', data)
+import { db } from "@/server/db"
 
+export const POST = async (req: Request) => {
+        const { data } = await req.json()
+        const emailAddress = data.email_addresses[0].email_addresses
+        const firstName = data.first_name
+        const lastName = data.last_name
+        const imageUrl = data.image_url
+        const id = data.id
+
+        await db.user.create({
+            data:{
+                id: id,
+                emailAddress: emailAddress,
+                firstName: firstName,
+                lastName: lastName,
+                imageUrl: imageUrl,
+            }
+        })
+        console.log('user created')
         return new Response('Webhook received', {status: 200})
     
 }
